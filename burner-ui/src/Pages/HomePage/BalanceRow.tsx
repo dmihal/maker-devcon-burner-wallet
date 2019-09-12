23b151ec59bd3ff82eb1e9cdc1a847d39a30c717
Alex Singh <alex@alexsingh.com>
2019-09-12 16:29:07 -0300

    Updates Balance object to use Rimble components. Starts matching to Figma styles

--- burner-ui/src/Pages/HomePage/BalanceRow.tsx
+++ burner-ui/src/Pages/HomePage/BalanceRow.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Asset } from '@burner-wallet/assets';
 const classes = require('./BalanceRow.module.css');
+import { Card, Box, Flex } from 'rimble-ui';
 
 interface BalanceRowProps {
   asset: Asset,
@@ -8,13 +9,21 @@ interface BalanceRowProps {
   balance?: string | null,
 }
 
+
 const BalanceRow: React.FC<BalanceRowProps> = ({ asset, usdBalance, balance }) => (
-  <li className={classes.balanceRow}>
-    <div className={classes.assetName}>{asset.name}</div>
-    <div className={classes.assetBalance}>
-      {!(usdBalance || balance) && '-'}
-      {usdBalance ? `$${usdBalance}` : balance}
-    </div>
+    <li className={classes.balanceRow}>
+      <Card maxWidth={160} borderRadius={4} p={3}>
+      <Flex flexDirection="row" justifyContent="space-between" alignContent="center">
+        <div class="circle"></div>
+          <Flex flexDirection="column" p={2}>
+          <div className={classes.assetBalance}>
+            {!(usdBalance || balance) && '-'}
+            {usdBalance ? `$${usdBalance}` : balance}
+          </div>
+          <div className={classes.assetName}>{asset.name}</div>
+          </Flex>
+      </Flex>
+    </Card>
   </li>
 );
 
