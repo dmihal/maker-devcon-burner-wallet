98169cc06900fdfab3ac5de7cc619965f0f2622a
Alex Singh <alex@alexsingh.com>
2019-09-18 13:57:39 -0300

    Incorporates homeButtons' and scanBtn into ActionRow and styles everything to the Figma design

--- burner-ui/src/Pages/HomePage/HomePage.tsx
+++ burner-ui/src/Pages/HomePage/HomePage.tsx
@@ -6,6 +6,8 @@ import Color from 'color';
 import { BurnerContext, withBurner } from '../../BurnerProvider';
 import Button from '../../components/Button';
 import Page from '../../components/Page';
+import ActionRow from '../../components/ActionRow';
+import { Card } from 'rimble-ui';
 import PluginElements from '../../components/PluginElements';
 import AccountBalance, { AccountBalanceData } from '../../data-providers/AccountBalance';
 import History from '../../data-providers/History';
@@ -23,21 +25,35 @@ const styles = (theme: any) => ({
     flexWrap: 'wrap',
   },
   buttonContainer: {
-    display: 'block',
     width: '50%',
-    padding: 4,
     boxSizing: 'border-box',
-    flex: '0 0 50%',
+    flex: '0 0 40%',
+    listStyle: 'none',
+  },
+  ActionRow: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    bottom: '16px',
+    display: 'flex',
+    padding: '8px 0px',
+    position: 'fixed',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    width: '95%',
+    borderRadius: '8px',
+    fontSize: '20px' // To Do: Change to CSS variable
   },
   homeButton: {
-    backgroundColor: theme.homeButtonColor,
+    color: '#4e3fce', // To Do: Change to CSS variable
+    backgroundColor: 'transparent',
   },
   scanBtn: {
     position: 'fixed',
-    bottom: 20,
-    right: 20,
-    height: 60,
-    width: 60,
+    left: '50%',
+    transform: 'translateX(-50%)',
+    bottom: -4,
+    height: 72,
+    width: 72,
     borderRadius: 100,
     backgroundImage: `url("${SCAN_QR_DATAURI}")`,
     backgroundSize: '60%',
@@ -67,20 +83,6 @@ const PK_REGEX = /^(?:0x)?[0-9a-f]{64}$/i;
 
 const HomePage: React.FC<BurnerContext & { classes: any }> = ({ accounts, actions, assets, pluginData, classes }) => (
   <Page>
-    <button className={classes.scanBtn} onClick={async () => {
-      try {
-        const result = await actions.scanQrCode();
-        if (pluginData.tryHandleQR(result, { actions })) {
-          return;
-        } else if (ADDRESS_REGEX.test(result)) {
-          actions.navigateTo('/send', { address: result });
-        } else if (PK_REGEX.test(result)) {
-          actions.callSigner('writeKey', accounts[0], result);
-        } else if (result.indexOf(location.origin) === 0) {
-          actions.navigateTo(result.substr(location.origin.length));
-        }
-      } catch (e) {}
-    }} />
 
     <PluginElements position="home-top" />
 
@@ -105,6 +107,7 @@ const HomePage: React.FC<BurnerContext & { classes: any }> = ({ accounts, action
 
     <PluginElements position="home-middle" />
 
+    {/*
     <ul className={classes.buttons}>
       <HomeButton path="/receive" title="Receive" classes={classes} />
       <HomeButton path="/send" title="Send" classes={classes} />
@@ -112,6 +115,7 @@ const HomePage: React.FC<BurnerContext & { classes: any }> = ({ accounts, action
         <HomeButton title={title} path={path} key={title} classes={classes} />
       ))}
     </ul>
+    */}
 
     {accounts.length > 0 && (
       <History
@@ -128,6 +132,28 @@ const HomePage: React.FC<BurnerContext & { classes: any }> = ({ accounts, action
     )}
 
     <PluginElements position="home-bottom" />
+
+    <Card className={classes.ActionRow}>
+      <HomeButton path="/receive" title="Request" classes={classes} />
+      <button className={classes.scanBtn} onClick={async () => {
+        try {
+          const result = await actions.scanQrCode();
+          if (pluginData.tryHandleQR(result, { actions })) {
+            return;
+          } else if (ADDRESS_REGEX.test(result)) {
+            actions.navigateTo('/send', { address: result });
+          } else if (PK_REGEX.test(result)) {
+            actions.callSigner('writeKey', accounts[0], result);
+          } else if (result.indexOf(location.origin) === 0) {
+            actions.navigateTo(result.substr(location.origin.length));
+          }
+        } catch (e) {}
+      }} />
+      <HomeButton path="/send" title="Send" classes={classes} />
+      {pluginData.homeButtons.map(({ title, path }) => (
+        <HomeButton title={title} path={path} key={title} classes={classes} />
+      ))}
+    </Card>
 
     <Link to="/advanced" className={classes.advancedLink}>Advanced</Link>
   </Page>
