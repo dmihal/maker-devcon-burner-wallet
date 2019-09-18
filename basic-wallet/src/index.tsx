ef6a9986bd4dd264f3567dc0835990aad14ed68b
David Mihal <david@dmihal.com>
2019-09-18 18:40:38 +0100

    Set up basic wallet on kovan

--- basic-wallet/src/index.tsx
+++ basic-wallet/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import { xdai, dai, eth } from '@burner-wallet/assets';
+import { xdai, dai, eth, NativeAsset } from '@burner-wallet/assets';
 import BurnerCore from '@burner-wallet/core';
 import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers';
 import { InfuraGateway, InjectedGateway, XDaiGateway, } from '@burner-wallet/core/gateways';
@@ -9,6 +9,7 @@ import Exchange from '@burner-wallet/exchange';
 import { xdaiBridge, uniswapDai } from '@burner-wallet/exchange/pairs';
 import BurnerUI from '../../burner-ui/src';
 import LegacyPlugin from '@burner-wallet/plugins/legacy';
+import CollectablePlugin from '../../collectable-plugin';
 
 // Github Pages hack
 if (localStorage.getItem('path')) {
@@ -23,18 +24,27 @@ const core = new BurnerCore({
     new InfuraGateway(process.env.REACT_APP_INFURA_KEY),
     new XDaiGateway(),
   ],
-  assets: [xdai, dai, eth],
+  // assets: [xdai, dai, eth],
+  assets: [new NativeAsset({
+    id: 'keth',
+    name: 'kETH',
+    network: '42',
+  })],
 });
 
-const exchange = new Exchange({
-  pairs: [xdaiBridge, uniswapDai],
-});
+// const exchange = new Exchange({
+//   pairs: [xdaiBridge, uniswapDai],
+// });
 
 const BurnerWallet = () =>
   <BurnerUI
     title="MakerDAO Wallet"
     core={core}
-    plugins={[exchange, new LegacyPlugin()]}
+    plugins={[
+      // exchange,
+      new LegacyPlugin(),
+      new CollectablePlugin('42', '0x0ACf5Ab7B4a80DEe293cC8DdE06b29C5798e2A72'),
+    ]}
   />
 
 
