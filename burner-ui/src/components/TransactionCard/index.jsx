6b989820e1221d52d161624060e4a789251a9b0a
Alex Singh <alex@alexsingh.com>
2019-09-21 14:21:27 -0300

    Adds TransactionCard as an independent component

---
+++ burner-ui/src/components/TransactionCard/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components'
+import { Card } from 'rimble-ui'
+
+
+
+export const TransactionCard = styled(Card)`
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px;
+  padding: 0px;
+  color: #4E3FCE;
+  background: #E1DEFF;
+`
+
+export const TransactionCardHeader = styled.div`
+  background: #CAC4FF;
+  padding: 16px;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+`
+
+export const TransactionCardBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const TransactionCardFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  width: 100%;
+  background: #D1CCFC;
+  padding: 8px;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  box-sizing: border-box;
+`
