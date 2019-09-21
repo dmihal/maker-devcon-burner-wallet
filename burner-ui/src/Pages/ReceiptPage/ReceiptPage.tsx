3f15daed375af0d3d2b60127c75f2e7e72496c2a
Carl Fairclough <hi@carlfairclough.me>
2019-09-21 11:20:54 +0400

    Updated ReceiptPage to reflect develop

--- burner-ui/src/Pages/ReceiptPage/ReceiptPage.tsx
+++ burner-ui/src/Pages/ReceiptPage/ReceiptPage.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
-import QRCode from 'qrcode.react';
-import Page from '../../components/Page';
-import LineItem from '../../components/LineItem';
-import TransactionDetails from '../../data-providers/TransactionDetails';
+import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import QRCode from "qrcode.react";
+import Page from "../../components/Page";
+import LineItem from "../../components/LineItem";
+import TransactionDetails from "../../data-providers/TransactionDetails";
 
 interface MatchParams {
   asset: string;
@@ -15,22 +15,27 @@ const ReceiptPage: React.FC<RouteComponentProps<MatchParams>> = ({ match }) => (
     <TransactionDetails
       asset={match.params.asset}
       txHash={match.params.txHash}
-      render={(err, tx) => {
-        if (err) {
-          return `Error: ${err.message}`;
-        }
-        if (!err && !tx) {
-          return 'Loading...';
+      render={tx => {
+        if (!tx) {
+          return (
+            <div>
+              <div>Transaction not found...</div>
+              <div>The transaction may still be propogating</div>
+            </div>
+          );
         }
 
         return (
           <div>
             <LineItem name="From" value={tx.from} />
             <LineItem name="To" value={tx.to} />
-            <LineItem name="Amount" value={`${tx.displayValue} ${tx.assetName}`} />
+            <LineItem
+              name="Amount"
+              value={`${tx.displayValue} ${tx.assetName}`}
+            />
             {tx.message && <LineItem name="Message" value={tx.message} />}
           </div>
-        )
+        );
       }}
     />
   </Page>
