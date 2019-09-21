9ea2d4a3617c45277d70d37940834d0536728088
Carl Fairclough <hi@carlfairclough.me>
2019-09-21 11:28:28 +0400

    Updated ConfirmPage to reflect develop

--- burner-ui/src/Pages/ConfirmPage/ConfirmPage.tsx
+++ burner-ui/src/Pages/ConfirmPage/ConfirmPage.tsx
@@ -1,28 +1,54 @@
-import React, { useState } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
-import { BurnerContext, withBurner } from '../../BurnerProvider';
-import Button from '../../components/Button';
-import Page from '../../components/Page';
-import LineItem from '../../components/LineItem';
+import React, { useState } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { BurnerContext, withBurner } from "../../BurnerProvider";
+import Button from "../../components/Button";
+import Page from "../../components/Page";
+import LineItem from "../../components/LineItem";
 
-const ConfirmPage: React.FC<BurnerContext & RouteComponentProps> = ({ history, assets, actions }) => {
+const ConfirmPage: React.FC<BurnerContext & RouteComponentProps> = ({
+  history,
+  assets,
+  actions,
+  pluginData
+}) => {
   if (!history.location.state) {
-    history.replace('/send');
+    history.replace("/send");
     return null;
   }
 
   const [sending, setSending] = useState(false);
 
-  const { to, from, ether, asset, message } = history.location.state;
-  const [_asset] = assets.filter(a => a.id === asset);
+  const {
+    to,
+    from,
+    ether,
+    asset: assetId,
+    message,
+    id
+  } = history.location.state;
+  const [asset] = assets.filter(a => a.id === assetId);
 
   const send = async () => {
     setSending(true);
     try {
-      actions.setLoading('Sending...');
-      const receipt = await _asset.send({ from, to, ether, message });
+      actions.setLoading("Sending...");
+      const receipt = await asset.send({ from, to, ether, message });
+
       actions.setLoading(null);
-      history.push(`/receipt/${_asset.id}/${receipt.transactionHash}`);
+      const redirect = pluginData.sent({
+        asset,
+        from,
+        to,
+        ether,
+        message,
+        receipt,
+        hash: receipt.transactionHash,
+        id
+      });
+
+      history.push(
+        redirect || `/receipt/${asset.id}/${receipt.transactionHash}`
+      );
     } catch (err) {
       setSending(false);
       console.error(err);
@@ -33,12 +59,16 @@ const ConfirmPage: React.FC<BurnerContext & RouteComponentProps> = ({ history, a
     <Page title="Confirm">
       <LineItem name="From" value={from} />
       <LineItem name="To" value={to} />
-      <LineItem name="Amount" value={`${ether} ${_asset.name}`} />
+      <LineItem name="Amount" value={`${ether} ${asset.name}`} />
       {message && <LineItem name="Message" value={message} />}
 
-      <div style={{ display: 'flex' }}>
-        <Button disabled={sending} onClick={send}>Send</Button>
-        <Button disabled={sending} onClick={() => history.goBack()}>Cancel</Button>
+      <div style={{ display: "flex" }}>
+        <Button disabled={sending} onClick={send}>
+          Send
+        </Button>
+        <Button disabled={sending} onClick={() => history.goBack()}>
+          Cancel
+        </Button>
       </div>
     </Page>
   );
