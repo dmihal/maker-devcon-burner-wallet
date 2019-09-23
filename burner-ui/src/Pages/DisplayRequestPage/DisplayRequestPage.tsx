0ab5b4ffae47c062e4d6fe220be86dc750fc5b71
Alex Singh <alex@alexsingh.com>
2019-09-23 19:45:40 -0300

    Updates the page to match Figma designs. WIP.

--- burner-ui/src/Pages/DisplayRequestPage/DisplayRequestPage.tsx
+++ burner-ui/src/Pages/DisplayRequestPage/DisplayRequestPage.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import QRCode from 'qrcode.react';
 import { BurnerContext, withBurner } from '../../BurnerProvider';
 import Page from '../../components/Page';
 import Button from '../../components/Button';
+import { TransactionCard,
+         TransactionCardHeader,
+         TransactionCardBody,
+         TransactionCardFooter }
+         from '../../components/TransactionCard';
+import Text from '../../components/Text';
 
 const QRWrapper = styled.div`
-width: 100%;
-max-width: 320px;
+  display: flex;
+  justify-content: center;
+  padding: 16px;
+  margin: 16px;
+  border: 1px solid #999;
+  border-radius: 8px;
 `
 
 const DisplayRequestPage = ({ history, actions }) => {
@@ -18,9 +28,18 @@ const DisplayRequestPage = ({ history, actions }) => {
   const { amount } = history.location.state;
 
   return (
-    <Page>
-      <QRCode value="0x0" />
-      <div>Amount: {amount}</div>
+    <Page title="Requesting" back>
+      <TransactionCard>
+        <TransactionCardHeader>
+        <Text level={3} as="h1" center>Please scan this QR code to complete the transaction.</Text>
+        </TransactionCardHeader>
+        <TransactionCardBody>
+          <QRWrapper>
+            <QRCode width={240} height={240} value="0x0" renderAs="svg"/>
+          </QRWrapper>
+          <Text level={3} as="h2">Amount: {amount}</Text>
+        </TransactionCardBody>
+      </TransactionCard>
       <Button onClick={() => actions.navigateTo('/request', { amount })}>Back</Button>
     </Page>
   );
