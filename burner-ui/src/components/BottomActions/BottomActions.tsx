1a43c3f6e8f8ce3300ff5d98187d185a94c248db
Carl Fairclough <hi@carlfairclough.me>
2019-09-24 15:04:48 +0400

    Added modals, used rimble

--- burner-ui/src/components/BottomActions/BottomActions.tsx
+++ burner-ui/src/components/BottomActions/BottomActions.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Box, Card, Flex } from 'rimble-ui';
 import styled from 'styled-components';
+import ReceiveModal from '../../Modals/Receive';
 import Button, { Link } from '../../components/Button';
 import { SCAN_QR_DATAURI } from '../../constants';
 
@@ -51,6 +52,26 @@ const BottomButton = styled(Link)`
   }
 `;
 
+// override default styles
+const BottomButtonButton = styled(Button)`
+  color: var(--color-primary);
+  background-color: transparent;
+  width: calc((100% - 72px - 8px) / 2);
+  z-index: 10;
+  border: none;
+  box-shadow: none;
+  &:hover,
+  &:focus,
+  &:active {
+    outline: none;
+    background: transparent;
+    box-shadow: none;
+  }
+  &:before {
+    display: none;
+  }
+`;
+
 interface BottomActionsProps {
   actions: any;
   pluginData: any;
@@ -58,39 +79,70 @@ interface BottomActionsProps {
   className?: string;
 }
 
-const BottomActions: React.FC<BottomActionsProps> = ({
-  actions,
-  pluginData,
-  defaultAccount,
-  className
-}) => (
-  <Box margin={'0 var(--page-margin)'} className={className}>
-    <Card width='auto' padding={0}>
-      <Flex justifyContent='space-between' alignItems='center'>
-        <BottomButton to='/receive' children='Request' shadow />
-        <ScanButton
-          onClick={async () => {
-            try {
-              const result = await actions.scanQrCode();
-              if (pluginData.tryHandleQR(result, { actions })) {
-                return;
-              } else if (ADDRESS_REGEX.test(result)) {
-                actions.navigateTo('/send', { address: result });
-              } else if (PK_REGEX.test(result)) {
-                actions.callSigner('writeKey', defaultAccount, result);
-              } else if (result.indexOf(location.origin) === 0) {
-                actions.navigateTo(result.substr(location.origin.length));
-              }
-            } catch (e) {}
-          }}
+class BottomActions extends Component<BottomActionsProps> {
+  constructor(props: BottomActionsProps) {
+    super(props);
+    this.state = {
+      receiveModalVisible: false
+    };
+  }
+
+  openModal = () => {
+    this.setState({
+      receiveModalVisible: true
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      receiveModalVisible: false
+    });
+  };
+
+  render() {
+    const { actions, pluginData, defaultAccount, className } = this.props;
+
+    return (
+      <>
+        <Box margin={'0 var(--page-margin)'} className={className}>
+          <Card width='auto' padding={0}>
+            <Flex justifyContent='space-between' alignItems='center'>
+              <BottomButtonButton
+                onClick={() => this.openModal()}
+                children='Request'
+                shadow
+              />
+              <ScanButton
+                onClick={async () => {
+                  try {
+                    const result = await actions.scanQrCode();
+                    if (pluginData.tryHandleQR(result, { actions })) {
+                      return;
+                    } else if (ADDRESS_REGEX.test(result)) {
+                      actions.navigateTo('/send', { address: result });
+                    } else if (PK_REGEX.test(result)) {
+                      actions.callSigner('writeKey', defaultAccount, result);
+                    } else if (result.indexOf(location.origin) === 0) {
+                      actions.navigateTo(result.substr(location.origin.length));
+                    }
+                  } catch (e) {}
+                }}
+              />
+              <BottomButton to='/send' children='Send' />
+            </Flex>
+          </Card>
+          {pluginData.homeButtons.map(({ title, path }) => (
+            <Link children={title} to={path} key={title} />
+          ))}
+        </Box>
+        <ReceiveModal
+          address={this.props.defaultAccount}
+          isOpen={this.state.receiveModalVisible}
+          hide={() => this.closeModal()}
         />
-        <BottomButton to='/send' children='Send' />
-      </Flex>
-    </Card>
-    {pluginData.homeButtons.map(({ title, path }) => (
-      <Link children={title} to={path} key={title} />
-    ))}
-  </Box>
-);
+      </>
+    );
+  }
+}
 
 export default BottomActions;
