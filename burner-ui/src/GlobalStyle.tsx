1e2c7b03a637ca94dc722b9b97f03abb64ffb182
Alex Singh <alex@alexsingh.com>
2019-09-25 05:01:57 -0300

    Fixing merge conflicts

--- burner-ui/src/GlobalStyle.tsx
+++ burner-ui/src/GlobalStyle.tsx
@@ -1,37 +1,39 @@
 import styled, { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-:root {
-  --l1-fs: 24px;
-  --l1-lh: 1.5;
-  --l1-weight: 600;
+  :root {
+    --l1-fs: 24px;
+    --l1-lh: 1.5;
+    --l1-weight: 600;
 
-  --l2-fs: 20px;
-  --l2-lh: 1.4;
-  --l2-weight: 400;
+    --l2-fs: 18px;
+    --l2-lh: 1.4;
+    --l2-weight: 600;
 
-  --l3-fs: 18px;
-  --l3-lh: 1.4;
-  --l3-weight: 400;
+    --l3-fs: 16px;
+    --l3-lh: 1.4;
+    --l3-weight: 400;
 
-  --l4-fs: 16px;
-  --l4-lh: 1.4;
-  --l4-weight: 400;
+    --l4-fs: 14px;
+    --l4-lh: 1.4;
+    --l4-weight: 400;
 
-  --l5-fs: 14px;
-  --l5-lh: 1.4;
-  --l5-weight: 400;
+    --l5-fs: 12px;
+    --l5-lh: 1.4;
+    --l5-weight: 400;
 
-  --main-font: system-ui, "-apple-system", BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    --main-font: system-ui, "-apple-system", BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
 
-  --page-margin: 16px;
+    --page-margin: 16px;
 
-  --color-primary:  #4E3FCE;
-  --color-secondary: #CAC4FF;
-  --color-tertiary: #E1DEFF;
+    --color-primary:  #4E3FCE;
+    --color-secondary: #CAC4FF;
+    --color-tertiary: #E1DEFF;
 
-}
 
+    --modal-background: #E1DEFF;
+    --modal-header-background: #CAC4FF;
+  }
 
   *, *:before, *:after {
     box-sizing: border-box;
