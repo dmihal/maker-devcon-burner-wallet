1714ea4a70a9dcb3e755ecbcb01e4b1326882bab
Alex Singh <alex@alexsingh.com>
2019-09-28 09:44:02 -0300

    Removes margin-top on Activity Page & removes border-top for the first-child HistoryListEvent

--- burner-ui/src/Pages/ActivityPage/ActivityPage.tsx
+++ burner-ui/src/Pages/ActivityPage/ActivityPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import HistoryEvent from '@burner-wallet/core/HistoryEvent';
 import { Box } from 'rimble-ui';
 import { BurnerContext, withBurner } from '../../BurnerProvider';
@@ -6,10 +7,16 @@ import Page from '../../components/Page';
 import History from '../../data-providers/History';
 import HistoryListEvent from '../../components/HistoryListEvent';
 
+const HistoryEventOnActivityPage = styled(HistoryListEvent)`
+  &:first-child {
+    border-top: 0px;
+}
+`
+
 const ActivityPage: React.FC<BurnerContext> = ({ defaultAccount, actions }) => {
   return (
     <Page title="Activity" back>
-      <Box mt={24} padding="0 var(--page-margin)" as="section">
+      <Box padding="0 var(--page-margin)" as="section">
         <History
           account={defaultAccount}
           render={(events: HistoryEvent[]) => events.slice(0, 3).map((event: HistoryEvent) => (
