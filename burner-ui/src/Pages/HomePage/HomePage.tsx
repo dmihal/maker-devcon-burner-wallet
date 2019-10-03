cc7b852423fc923e1bb1ae53393a8cffcc120eee
Alex Singh <alex@alexsingh.com>
2019-10-03 13:18:05 +0200

    Adds WIP AppIcon component

--- burner-ui/src/Pages/HomePage/HomePage.tsx
+++ burner-ui/src/Pages/HomePage/HomePage.tsx
@@ -6,7 +6,7 @@ import Button from '../../components/Button';
 import Page from '../../components/Page';
 import styled from 'styled-components';
 // import ActionRow from '../../components/ActionRow';
-import { Box, Flex } from 'rimble-ui';
+import { Box, Flex, Card } from 'rimble-ui';
 import PluginElements from '../../components/PluginElements';
 import History from '../../data-providers/History';
 import BalanceRow from '../../components/BalanceRow';
@@ -38,12 +38,29 @@ const ViewAllButton = styled(Link)`
   display: flex;
   align-items: center;
   color: #555;
-  padding: 8px 12px;  
+  padding: 8px 12px;
   text-decoration: none;
 
   &:after {
     content: '\\203A';
     margin-left: 10px;
+  }
+`;
+
+const AppCard = styled(Card)`
+  display: flex;
+  flex: 1;
+  border-radius: 8px;
+  padding: 16px;
+  padding-right: 12px;
+  margin: 8px 0px;
+  font-weight: 400;
+  align-items: center;
+  &:not(:first-of-type) {
+    margin-left: 12px;
+  }
+  & > span {
+    display: block;
   }
 `;
 
@@ -90,6 +107,19 @@ class HomePage extends Component<BurnerContext & HomePageProps, any> {
           />
         </Box>
         <Box margin='0 var(--page-margin)'>
+          <AppCard>
+          <Box width={'60%'}>
+            <Text level={2} as={'h3'} margin={'0'} color={'#444'}>
+              Sablier
+            </Text>
+            <Text level={3} as={'p'} margin={'0'} textAlign={'left'} color={'#999'}>
+              Continuous payment streams you can access instantly.
+            </Text>
+          </Box>
+            <Flex width={'40%'} flexDirection={'column'} alignItems={'center'}>
+              <Box borderRadius={100} width={'64px'} height={'64px'} backgroundColor={'#999999'}></Box>
+            </Flex>
+          </AppCard>
           <Link to='/advanced'>Advanced</Link>
         </Box>
         <PositionedBottomActions
