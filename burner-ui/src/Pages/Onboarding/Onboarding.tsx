529131026148d8dcc123b4452b1f6e039f22868d
Alex Singh <alex@alexsingh.com>
2019-10-04 22:05:06 +0900

    Added Maker logo to onboarding screen

--- burner-ui/src/Pages/Onboarding/Onboarding.tsx
+++ burner-ui/src/Pages/Onboarding/Onboarding.tsx
@@ -7,6 +7,8 @@ import { Redirect } from 'react-router-dom';
 const H1 = styled.h1`
   font-weight: 600;
   font-size: 3.3ch;
+  margin: 0;
+  color: ${props => props.color}
 `;
 
 const P = styled.p`
@@ -42,6 +44,13 @@ const BigEmoji = styled.div`
   -moz-animation: bounce 2s infinite;
   -o-animation: bounce 2s infinite;
 `;
+
+const MakerLogo = () => (
+<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M21.4813 33.9651C20.7927 33.9651 20.2854 33.4721 20.2854 32.8383V23.9297L10.3918 17.3099V32.8383C10.3918 33.4721 9.84824 33.9651 9.19592 33.9651C8.5436 33.9651 8 33.4721 8 32.8383V15.1268C8 14.493 8.5436 14 9.19592 14C9.4496 14 9.70328 14.0704 9.88448 14.2113L22.1699 22.486C22.496 22.6973 22.6772 23.0142 22.6772 23.4015V32.8735C22.6772 33.4721 22.1336 33.9651 21.4813 33.9651Z" fill="#1AAB9B"/>
+<path d="M26.5187 33.9648C25.8301 33.9648 25.3228 33.4718 25.3228 32.838V23.4012C25.3228 23.0491 25.5402 22.697 25.8301 22.4857L38.1155 14.2462C38.3329 14.1053 38.5504 14.0349 38.804 14.0349C39.3839 14.0349 40 14.4575 40 15.1617V32.8732C40 33.507 39.4564 34 38.804 34C38.1517 34 37.6081 33.507 37.6081 32.8732V17.3448L27.7146 23.9294V32.838C27.7146 33.4718 27.171 33.9648 26.5187 33.9648Z" fill="#1AAB9B"/>
+</svg>
+);
 
 const BounceAnimation = createGlobalStyle`
 
@@ -91,8 +100,15 @@ const Step1: React.FC<Step1Props> = ({ next }) => {
     <>
       <Box mt='auto' mb='auto'>
         <H1>
-          Claim your <br />
-          Maker <br />burner wallet
+          Claim your
+        </H1>
+        <Flex alignItems={'center'} my={3}>
+        <MakerLogo />
+        <H1 margin={'0'} color={'var(--color-primary)'}>
+        Maker
+        </H1>
+        </Flex>
+        <H1>burner wallet
         </H1>
         <P>If you are in incognito mode, please enter regular browsing mode.</P>
       </Box>
@@ -122,7 +138,7 @@ const Step2: React.FC<Step2Props> = ({ next }) => {
         <IcoArrow>{'\u2192'}</IcoArrow>Find and scan physical Dai tokens to load up on xDai
       </P>
       <P>
-        <IcoArrow>{'\u2192'}</IcoArrow>Purchase swag from the Maker booth.
+        <IcoArrow>{'\u2192'}</IcoArrow>Use your xDai to buy swag from the Maker booth.
       </P>
       <P>
         <IcoArrow>{'\u2192'}</IcoArrow>Look for special QR codes scattered around Devcon to get rare NFTs
